ea369eb8fd03a2cc248eb5f0239f4f6ca7a0d6bc
jaren <jarensmart@yahoo.com>
2020-06-04 00:54:02 -0400

    reset package.json && removed try/catch blocks for else/if

--- README.md
+++ README.md
@@ -45,37 +45,37 @@ Each User _resource_ should conform to the following structure (AKA schema):
 
 #### Endpoint Specifications
 
-When the client makes a `POST` request to `/api/users`:
+[x] When the client makes a `POST` request to `/api/users`:
 
-- If the request body is missing the `name` or `bio` property:
+- If the request body is missing the `name` or `bio` property: [x]
 
   - respond with HTTP status code `400` (Bad Request).
   - return the following JSON response: `{ errorMessage: "Please provide name and bio for the user." }`.
 
-- If the information about the _user_ is valid:
+- If the information about the _user_ is valid: [x]
 
   - save the new _user_ the the database.
   - respond with HTTP status code `201` (Created).
   - return the newly created _user document_.
 
-- If there's an error while saving the _user_:
+- If there's an error while saving the _user_: [x]
   - respond with HTTP status code `500` (Server Error).
   - return the following JSON object: `{ errorMessage: "There was an error while saving the user to the database" }`.
 
-When the client makes a `GET` request to `/api/users`:
+[x] When the client makes a `GET` request to `/api/users`:
 
 - If there's an error in retrieving the _users_ from the database:
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ errorMessage: "The users information could not be retrieved." }`.
 
-When the client makes a `GET` request to `/api/users/:id`:
+[x] When the client makes a `GET` request to `/api/users/:id`:
 
-- If the _user_ with the specified `id` is not found:
+- If the _user_ with the specified `id` is not found: [x]
 
   - respond with HTTP status code `404` (Not Found).
   - return the following JSON object: `{ message: "The user with the specified ID does not exist." }`.
 
-- If there's an error in retrieving the _user_ from the database:
+- If there's an error in retrieving the _user_ from the database: [x]
   - respond with HTTP status code `500`.
   - return the following JSON object: `{ errorMessage: "The user information could not be retrieved." }`.
 
---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const db = require("./database.js");
+
+const server = express();
+
+//install middleware to allow express
+server.use(express.json());
+
+//USERS
+server.get("/", (req, res) => {
+  const users = db.getUsers();
+
+  if (users) {
+    res.json(users);
+  } else {
+    res.status(500).json({
+      errorMessage: "The users information could not be retrieved.",
+    });
+  }
+});
+
+//USERS BY ID
+server.get("/users/:id", (req, res) => {
+  const userByID = req.params.id;
+  const user = db.getUserById(userByID);
+
+  if (user) {
+    res.json(user);
+  } else if (!user) {
+    res.status(404).json({
+      message: "The user with the specified ID does not exist.",
+    });
+  } else {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved.",
+    });
+  }
+});
+
+//ADDING NEW USER
+server.post("/users", (req, res) => {
+  if (!req.body.name || !req.body.bio) {
+    return res.status(400).json({
+      errorMessage: "Please provide name and bio for the user.",
+    });
+  }
+
+  const newUser = db.createUser({
+    name: req.body.name,
+    bio: req.body.bio,
+  });
+
+  if (newUser) {
+    res.status(201).json(newUser);
+  } else {
+    res.status(500).json({
+      errorMessage: "There was an error while saving the user to the database",
+    });
+  }
+});
+
+//UPDATE USER
+server.put("/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+
+  // can't update a user that doesn't exist, so check first
+  if (user) {
+    const updatedUser = db.updateUser(user.id, {
+      // we whitelist values here instead of passing req.body directly,
+      // so we know exactly what's allowed to be updated and what's not.
+      name: req.body.name || user.name,
+    });
+
+    res.json(updatedUser);
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+});
+
+//DELETE USER
+server.delete("/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id);
+
+  if (user) {
+    db.deleteUser(user.id);
+    // 204 is just a successful empty response,
+    // since we don't really have anything to return
+    res.status(204).end();
+  } else {
+    res.status(404).json({
+      message: "User not found",
+    });
+  }
+});
+
+server.listen(7777, () => {
+  console.log("server is listening on port 7777");
+});
